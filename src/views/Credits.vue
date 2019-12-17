35b502c200fd0b490af2d7e2a85c2d99e7128deb
Lokesh Dhakar <lokesh.dhakar@gmail.com>
2019-12-16 21:23:50 -0800

    fix: missing div in Credits

--- src/views/Credits.vue
+++ src/views/Credits.vue
@@ -1,9 +1,8 @@
 <template>
-    <div class="credits">
-      <div class="credits__author">by Lokesh Dhakar</div>
-      <a href="https://twitter.com/lokesh" class="credits__link">Twitter</a> -
-      <a href="https://lokeshdhakar.com" class="credits__link">Website</a>
-    </div>
+  <div class="credits">
+    <div class="credits__author">by Lokesh Dhakar</div>
+    <a href="https://twitter.com/lokesh" class="credits__link">Twitter</a> -
+    <a href="https://lokeshdhakar.com" class="credits__link">Website</a>
   </div>
 </template>
 
